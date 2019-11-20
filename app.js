3bbe7b1f274e8e0ae193a0e9328d32ce019b0dd6
Zeeshan <mdzeeshanali93@gmail.com>
2019-11-20 18:23:42 +0530

    rate limit increased

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ if (process.env.NODE_ENV === "development") {
 
 //Limit request from same IP
 const limiter = rateLimit({
-  max: 250,
+  max: 500,
   windowMs: 60 * 60 * 1000,
   message: "Too many requests from this IP, please try again in an hour!"
 });
